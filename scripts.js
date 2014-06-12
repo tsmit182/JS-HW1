ba973879f10d6fde2c8f16b740d7058d479304e8
Taylor Smith <tsmit182@gmail.com>
2014-06-12 17:42:57 -0400

    Add some JavaScript

--- scripts.js
+++ scripts.js
@@ -1,12 +1,41 @@
-firstName = "Taylor"
+// firstName = "Taylor"
+// console.log(firstName)
 
-console.log(firstName)
+// lastName = "Smith"
+// console.log(lastName)
 
-lastName = "Smith"
+// age = 2014 - 1986
+// console.log(age)
 
-console.log(lastName)
+// favColor = "blue"
+// console.log(favColor)
 
-age = 2014 - 1986
+// voluntarist = true
+// console.log(voluntarist)
 
-console.log(age)
 
+// var truthStatement = "A equals A"
+// .split(truthStatement)
+
+// var anotherStatement = "Shoop de doop-doop de da, daaaa."
+// .concat(anotherStatement)
+
+// 		aString = "Hello, I am a string."
+// 		.concat(aString)
+
+// var thirdStatement = "'You’ve been all over the world, you’ve hunted just about every kind of game, you’ve stalked elephant, lions, tigers, polar bears, deer, you’ve been within 15, 20 yards the most dangerous game in the world. You’ve been everywhere. You’ve been with some of the most famous guides and hunters in the world. What’s the biggest thrill you’ve ever had out of all of that?' 'Well, I think I can answer that best by calling attention to the ...'
+
+// 'Bazooka Tooth, bitch.'" 
+// thirdStatement.length
+
+
+
+// myArray = [1, 2, 4, 8, 16, 32, "doubling", "numbers", "awesome", "mostly even"]
+
+// myArray.length
+
+// myArray.push()
+// myArray.pop()
+// myArray.join()
+// myArray.reverse()
+// myArray.splice()
